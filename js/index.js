5d4b2e7450bc2e4eccd743b16ec873f07405be18
happyday <jsoibn@gmail.com>
2022-06-28 16:11:15 +0700

    Fixed: header

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
 $(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(this).scrollTop()) {
-            $('header').addClass("onScroll");
-        }
-        else{
-            $('header').removeClass("onScroll");
-        }
-    });
-});
+  $(window).scroll(function () {
+    if ($(this).scrollTop()) {
+      $("header").addClass("onScroll")
+    } else {
+      $("header").removeClass("onScroll")
+    }
+  })
+})
+
+// hàm bật cái menu
+function dropDownMenu() {
+  // dom tới cái meunu_item
+  document.getElementById("drop-down-content").style.display = "block"
+  // xong style.display = "block" là xong
+}
