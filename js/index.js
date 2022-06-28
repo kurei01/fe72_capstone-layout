af501b4207273934f447ed08238762320b9afe7e
kurei01 <ku12322132@gmail.com>
2022-06-29 03:17:56 +0700

    blog-post still pagnigation

--- js/index.js
+++ js/index.js
@@ -3,8 +3,15 @@ $(document).ready(function () {
         if ($(this).scrollTop()) {
             $('header').addClass("onScroll");
         }
-        else{
+        else {
             $('header').removeClass("onScroll");
         }
     });
 });
+
+// hàm bật cái menu
+function dropDownMenu() {
+    // dom tới cái meunu_item
+    document.getElementById("drop-down-content").style.display = "block"
+    // xong style.display = "block" là xong
+}
