e4b6e96430d2d7b90477502bc239cea371fcc15a
kurei01 <ku12322132@gmail.com>
2022-07-01 16:50:23 +0700

    done, wait more respon

--- js/index.js
+++ js/index.js
@@ -15,3 +15,29 @@ function dropDownMenu() {
     document.getElementById("drop-down-content").style.display = "block"
     // xong style.display = "block" là xong
 }
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("movetop").style.display = "block";
+    } else {
+        document.getElementById("movetop").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+$(function () {
+    $('.navbar-toggler').click(function () {
+        $('body').toggleClass('noscroll');
+    })
+});
