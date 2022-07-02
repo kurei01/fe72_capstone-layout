3d82c61e50ff3d6239999968d92537e3eb450246
kurei01 <ku12322132@gmail.com>
2022-07-02 09:48:00 +0700

    ready to merge

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
   $(window).scroll(function () {
-    if ($(this).scrollTop()) {
-      $("header").addClass("onScroll")
-    } else {
-      $("header").removeClass("onScroll")
-    }
-  })
-})
+      if ($(this).scrollTop()) {
+          $('header').addClass("onScroll");
+      }
+      else {
+          $('header').removeClass("onScroll");
+      }
+  });
+});
 
 // hàm bật cái menu
 function dropDownMenu() {
@@ -14,3 +15,29 @@ function dropDownMenu() {
   document.getElementById("drop-down-content").style.display = "block"
   // xong style.display = "block" là xong
 }
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("movetop").style.display = "block";
+  } else {
+      document.getElementById("movetop").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+$(function () {
+  $('.navbar-toggler').click(function () {
+      $('body').toggleClass('noscroll');
+  })
+});
